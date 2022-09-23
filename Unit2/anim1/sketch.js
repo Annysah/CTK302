026be1bb9a6a2b5df5e452dc446bcaf8954f8581
Anisat Akinbani <52763841+Annysah@users.noreply.github.com>
2022-09-23 15:05:46 -0500

    update anim1 code library

--- Unit2/anim1/sketch.js
+++ Unit2/anim1/sketch.js
@@ -11,7 +11,10 @@ function setup() {
 }
 
 function draw() {
-  background("orange");
+  let color1 = color("#33ccff");
+  let color2 = color("#ff99cc");
+
+  setGradient(0, 0, width, height, color1, color2, "Y");
 
   fill("#fff");
 
@@ -22,5 +25,25 @@ function draw() {
 
   if(x > width) {
     x = -1000
+  }
+}
+
+//A function from the p5js LinearGradient example
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+  if (axis == "Y") {
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis == "X") {
+    for (let j = x; j <= x + w; j++) {
+      let inter2 = map(j, x, x + w, 0, 1);
+      let d = lerpColor(c1, c2, inter2);
+      stroke(d);
+      line(j, y, j, y + h);
+    }
   }
 }
